cef913e0a0a697b9f54f63fa74d2e94f2145d30d
s6k6y <863671520@qq.com>
2023-05-16 12:36:43 +0800

    first change

--- src/com/tcx/GitDemo.java
+++ src/com/tcx/GitDemo.java
@@ -4,5 +4,7 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello");
     }
-
+    public void Test(){
+        System.out.println("lalala");
+    }
 }
